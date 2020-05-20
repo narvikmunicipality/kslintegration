e6c5a5076a0b6eda087012e4c280dc51f9850f23
Bjørn-Tore <bjorn.tore.lilleng@narvik.kommune.no>
2020-05-20 13:05:07 +0200

    Rerouted URLs.

--- spec/routes/index_spec.js
+++ spec/routes/index_spec.js
@@ -24,40 +24,40 @@ describe('routes-index', () => {
     });
 
     describe('maps', () => {
-        it('/v3/authority to AuthorityController', () => {
+        it('/ksl/v3/authority to AuthorityController', () => {
             routes(containerMock);
 
-            expect(routerMock.get).toHaveBeenCalledWith('/v3/authority', containerMock.AuthorityController.get);
+            expect(routerMock.get).toHaveBeenCalledWith('/ksl/v3/authority', containerMock.AuthorityController.get);
         });
 
-        it('/v3/employee_position to EmployeePositionController', () => {
+        it('/ksl/v3/employee_position to EmployeePositionController', () => {
             routes(containerMock);
 
-            expect(routerMock.get).toHaveBeenCalledWith('/v3/employee_position', containerMock.EmployeePositionController.get);
+            expect(routerMock.get).toHaveBeenCalledWith('/ksl/v3/employee_position', containerMock.EmployeePositionController.get);
         });
 
-        it('/v3/employee_taxanomy to EmployeeTaxonomyController', () => {
+        it('/ksl/v3/employee_taxanomy to EmployeeTaxonomyController', () => {
             routes(containerMock);
 
-            expect(routerMock.get).toHaveBeenCalledWith('/v3/employee_taxonomy', containerMock.EmployeeTaxonomyController.get);
+            expect(routerMock.get).toHaveBeenCalledWith('/ksl/v3/employee_taxonomy', containerMock.EmployeeTaxonomyController.get);
         });
         
-        it('/v3/organisation to OrganisationController', () => {
+        it('/ksl/v3/organisation to OrganisationController', () => {
             routes(containerMock);
 
-            expect(routerMock.get).toHaveBeenCalledWith('/v3/organisation', containerMock.OrganisationController.get);
+            expect(routerMock.get).toHaveBeenCalledWith('/ksl/v3/organisation', containerMock.OrganisationController.get);
         });
 
-        it('/v3/person to PersonController', () => {
+        it('/ksl/v3/person to PersonController', () => {
             routes(containerMock);
 
-            expect(routerMock.get).toHaveBeenCalledWith('/v3/person', containerMock.PersonController.get);
+            expect(routerMock.get).toHaveBeenCalledWith('/ksl/v3/person', containerMock.PersonController.get);
         });
 
-        it('/v3/venue to VenueController', () => {
+        it('/ksl/v3/venue to VenueController', () => {
             routes(containerMock);
 
-            expect(routerMock.get).toHaveBeenCalledWith('/v3/venue', containerMock.VenueController.get);
+            expect(routerMock.get).toHaveBeenCalledWith('/ksl/v3/venue', containerMock.VenueController.get);
         });
     });
 });
