399eb3c2e2ef5bcf01718bf962bbb4d71159413b
Bjørn-Tore <bjorn.tore.lilleng@narvik.kommune.no>
2020-07-20 08:43:28 +0200

    Formatting.

--- spec/helper/VismaDataExtractor_spec.js
+++ spec/helper/VismaDataExtractor_spec.js
@@ -1,7 +1,7 @@
 describe('VismaDataExtractor', () => {
     const VismaDataExtractor = require('../../src/helper/VismaDataExtractor')
     const EMPLOYEE_WITH_TWO_POSITIONS = { familyName: 'FamilyName', givenName: 'GivenName', ssn: '01020304050', employeeId: '11', positions: [{ isManagerPosition: true, isPrimaryPosition: true, startDate: '2020-02-01', organisationId: '101', unitId: '1001', unitName: 'Enhetsnavn 1', name: 'Konsulent' }, { isManagerPosition: false, isPrimaryPosition: false, startDate: '2020-02-01', organisationId: '102', unitId: '1002', unitName: 'Enhetsnavn 2', name: 'Ingeniør' }] }
-    const SSN_TO_MAIL_MAP = [{ssn: '01020304050', mail: 'Mail1'}]
+    const SSN_TO_MAIL_MAP = [{ ssn: '01020304050', mail: 'Mail1' }]
     let extractor
 
     beforeEach(() => {
@@ -30,10 +30,10 @@ describe('VismaDataExtractor', () => {
         it('sets dummy address when missing address', () => {
             expect(extractor.Person([]).createMap(EMPLOYEE_WITH_TWO_POSITIONS, 1)).toEqual({ SocialSecurityNumber: '01020304050', FirstName: 'GivenName', LastName: 'FamilyName', EmployeeId: '11', Email: 'har.ikke.e-post@example.com' })
         })
-        
+
         it('sets dummy address when address is whitespace', () => {
-            expect(extractor.Person([{ssn: '01020304050', mail: ' '}]).createMap(EMPLOYEE_WITH_TWO_POSITIONS, 1)).toEqual({ SocialSecurityNumber: '01020304050', FirstName: 'GivenName', LastName: 'FamilyName', EmployeeId: '11', Email: 'har.ikke.e-post@example.com' })
-        })    
+            expect(extractor.Person([{ ssn: '01020304050', mail: ' ' }]).createMap(EMPLOYEE_WITH_TWO_POSITIONS, 1)).toEqual({ SocialSecurityNumber: '01020304050', FirstName: 'GivenName', LastName: 'FamilyName', EmployeeId: '11', Email: 'har.ikke.e-post@example.com' })
+        })
     })
 
     describe('extracts and maps values correctly for EmployeePosition table', () => {
